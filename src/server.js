4a8d909b91b80569036cd3bcbcdd6487d4549576
chanchal16 <rajputchanchal17@gmail.com>
2025-04-09 16:48:14 +0530

    update: add tasks routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require('./routes/taskRoutes')
 
 dotenv.config();
 connectDB();
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server running on port:${PORT}`));
